9e8c4c10d9fe40ee8cbe0a03f6cd72910062c43a
mdme234 <kiokoabundance@gmail.com>
2022-10-21 17:06:26 +0300

    change media url

--- julio/settings.py
+++ julio/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kings-daughter.herokuapp.com','127.0.0.1']
 
@@ -133,7 +133,9 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 # STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog/static'),
+]
 
 # aws settings
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
@@ -144,7 +146,7 @@ AWS_DEFAULT_ACL = 'public-read'
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-DEFAULT_FILE_STORAGE ='blog.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = f'julio.storage_backends.MediaStorage'
 
 # s3 static settings
 AWS_LOCATION = 'static'
@@ -160,21 +162,13 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #     'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 # )
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'blog/media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'blog/media')
 
 # LOGIN_REDIRECT_URL = 'index'
 
 # LOGIN_URL = 'users:site-login'
-
-#  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
-
 
 
 
