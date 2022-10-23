5240fa69812e50809f5257319c4ccec88cf99dab
mdme234 <kiokoabundance@gmail.com>
2022-10-23 18:49:30 +0300

    change log url

--- julio/settings.py
+++ julio/settings.py
@@ -167,7 +167,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 
-LOGIN_REDIRECT_URL = 'index'
+# LOGIN_REDIRECT_URL = 'index'
 
 # LOGIN_URL = 'users:site-login'
 
